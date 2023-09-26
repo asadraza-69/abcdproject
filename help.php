4d89adf161bb0f0ff0ce876d8cef2bb9e31262e0
M.Asad Ali Akbar <m.asad@bitswits.com>
2023-09-26 11:15:35 +0500

    update help

--- help.php
+++ help.php
@@ -14,33 +14,73 @@
   <h2>Frequently Asked Questions</h2>
   <div class="accordion">
     <div class="accordion-item">
-      <button id="accordion-button-1" aria-expanded="false"><span class="accordion-title">Why is the moon sometimes out during the day?</span><span class="icon" aria-hidden="true"></span></button>
+      <button id="accordion-button-1" aria-expanded="false"><span class="accordion-title">What is the purpose of Dress Numbers?</span><span class="icon" aria-hidden="true"></span></button>
       <div class="accordion-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+        <p>The "Dress Numbers" textbox serves as a user-input field where you can manage and input dress IDs. These dress IDs are used generate book of your desired dress.</p>
       </div>
     </div>
     <div class="accordion-item">
-      <button id="accordion-button-2" aria-expanded="false"><span class="accordion-title">Why is the sky blue?</span><span class="icon" aria-hidden="true"></span></button>
+      <button id="accordion-button-2" aria-expanded="false"><span class="accordion-title">Can I modify the dress IDs in the textbox?</span><span class="icon" aria-hidden="true"></span></button>
       <div class="accordion-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+        <p>Yes, you have full control over the dress IDs within the text box. You can add new dress IDs, remove existing ones, or copy and paste a new list of dress IDs as needed.</p>
       </div>
     </div>
     <div class="accordion-item">
-      <button id="accordion-button-3" aria-expanded="false"><span class="accordion-title">Will we ever discover aliens?</span><span class="icon" aria-hidden="true"></span></button>
+      <button id="accordion-button-3" aria-expanded="false"><span class="accordion-title">How does the "Generate" button work in relation to the "Dress Numbers" textbox?</span><span class="icon" aria-hidden="true"></span></button>
       <div class="accordion-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+        <p>The "Generate" button relies on the dress IDs present in the "Dress Numbers" text box. When you click the "Generate" button, it uses the dress IDs from the textbox to perform actions and create book of desire dress IDs.</p>
       </div>
     </div>
     <div class="accordion-item">
-      <button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">How much does the Earth weigh?</span><span class="icon" aria-hidden="true"></span></button>
+      <button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">What are the available layout options?</span><span class="icon" aria-hidden="true"></span></button>
       <div class="accordion-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+        <p>There are three layout options choose.</p>
       </div>
     </div>
     <div class="accordion-item">
-      <button id="accordion-button-5" aria-expanded="false"><span class="accordion-title">How do airplanes stay up?</span><span class="icon" aria-hidden="true"></span></button>
+      <button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">How do I select a layout option?</span><span class="icon" aria-hidden="true"></span></button>
       <div class="accordion-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+        <p> You can select a layout option by checking the corresponding radio button next to your preferred choice. Click on the radio button that matches the layout you want to use.</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">What is the difference between these layout options?</span><span class="icon" aria-hidden="true"></span></button>
+      <div class="accordion-content">
+        <p>Each layout option has a different arrangement for displaying pictures and text:<br/>
+<strong>Picture on the left side, text on the right side, on a single page in landscape mode.</strong>
+<br/>
+<strong>Picture on the right side, text on the left side, on a single page in landscape mode.</strong>
+<br/>
+<strong>Picture on the left page, text on the right page, in a two-page layout in portrait mode.<strong>
+</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <button id="accordion-button-4" aria-expanded="false"><span class="accordion-title">What is the difference between these sort order options?</span><span class="icon" aria-hidden="true"></span></button>
+      <div class="accordion-content">
+        <p>Each sort order option determines the sequence in which items are arranged or displayed:<br/>
+        <strong>By Name:</strong> Items are sorted alphabetically by their names.<br/>
+        <strong>By ID:</strong> Items are sorted numerically or alphabetically by their unique identifiers.<br/>
+        <strong>By Input Order:</strong> Items retain the order in which they were originally input or added..</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <button id="accordion-button-5" aria-expanded="false"><span class="accordion-title">What are the available numbering options?</span><span class="icon" aria-hidden="true"></span></button>
+      <div class="accordion-content">
+        <p>There are three numbering options to choose from:<br/>
+<strong>Show both Page No and Dress ID</strong><br/>
+<strong>Show Page number</strong><br/>
+<strong>Show Dress ID</strong>
+</p>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <button id="accordion-button-5" aria-expanded="false"><span class="accordion-title">What is the difference between these numbering options?</span><span class="icon" aria-hidden="true"></span></button>
+      <div class="accordion-content">
+        <p>
+<strong>Show both Page No and Dress ID:</strong> Both page numbers and dress IDs are displayed together.<br/>
+<strong>Show Page number:</strong> Only page numbers are displayed.<br/>
+<strong>Show Dress ID:</strong> Only dress IDs are displayed.</p>
       </div>
     </div>
   </div>
